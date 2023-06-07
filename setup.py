a0b535b44348a03ef81813f47d55fcb8416c9497
Franz Waibl <waiblfranz@gmail.com>
2023-06-07 16:27:56 +0200

    Specify ANARCI commit in setup.py, add pdb2pqr to dependencies.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     packages=["surface_analyses", "surface_analyses.anarci_wrapper"],
     include_package_data=True,
     zip_safe=False,
-    install_requires=['numpy', 'scipy', 'pandas', 'scikit-image', 'gisttools @ git+https://github.com/liedllab/gisttools.git', 'plyfile', 'matplotlib>=3.7', 'anarci @ git+https://github.com/oxpig/ANARCI', 'biopython', 'rdkit'],
+    install_requires=['numpy', 'scipy', 'pandas', 'scikit-image', 'gisttools @ git+https://github.com/liedllab/gisttools.git', 'plyfile', 'matplotlib>=3.7', 'anarci @ git+https://github.com/oxpig/ANARCI@b22f2ee05f4fc241f558b8ccb0c522d4e7563b30', 'pdb2pqr', 'biopython', 'rdkit'],
     setup_requires=['pytest_runner'],
     tests_require=['pytest'],
     py_modules=[
